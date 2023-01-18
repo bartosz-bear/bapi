d97fba48fccb36e2b97f884320729ef7d3207fa6
bartosz-bear <bartosz.artur.piechnik@gmail.com>
2023-01-18 12:20:11 +0100

    add templates; use shortcuts in urls and views; create requirements.txt

--- bapi_django/bapi_django_app/urls.py
+++ bapi_django/bapi_django_app/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'bapi_django_app'
 urlpatterns = [
-  path('', views.index, name='index')
+  # /bapi_django_app
+  path('', views.index, name='index'),
+  # eg. /bapi_django_app/5/
+  path('<int:question_id>/', views.detail, name='detail'),
+  # eg. /bapi_django_app/5/results/
+  path('<int:question_id>/results/', views.results, name='results'),
+  # ex. /bapi_django_app/vote/
+  path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- bapi_django/bapi_django_app/views.py
+++ bapi_django/bapi_django_app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  #template = loader.get_template('bapi_django_app/index.html')
+  context = {
+    'latest_question_list': latest_question_list
+  }
+  return render(request, 'bapi_django_app/index.html', context)
+
+
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'bapi_django_app/detail.html', {'question': question})
+
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
+
