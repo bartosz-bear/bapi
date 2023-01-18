98e675da55fb2d921bec3e4e30f4aa8ff605f1b4
bartosz-bear <bartosz.artur.piechnik@gmail.com>
2023-01-18 21:38:33 +0100

    add scaffolding of index.html based on bootstrap5

--- bapi_django/bapi_django/settings.py
+++ bapi_django/bapi_django/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('PRODUCTION_HOST'),
+                 config('LOCAL_HOST')
+                ]
 
 
 # Application definition
