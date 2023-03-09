25d955741ac4fcbec6b89eacf53401f8bced2afe
bartosz-bear <bartosz.artur.piechnik@gmail.com>
2023-03-09 19:13:56 +0100

    adding utility function to format requirments.txt; updating requirements.txt

--- bapi_django/bapi_django/settings/prod.py
+++ bapi_django/bapi_django/settings/prod.py
@@ -2,3 +2,5 @@ from decouple import config
 
 DEBUG = False
 ALLOWED_HOSTS = [config('PRODUCTION_HOST')]
+SESION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
---
+++ bapi_django/bapi_django/utils/utils.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+filename = Path(__file__).resolve().parent.parent.parent.parent.__str__() + "\\requirements.txt"
+
+def drop_builds(x):
+    a = '='.join(x.split('=')[:2])
+    return a
+
+# Read requirements.txt and create a list of libraries
+with open(filename, 'r') as file:
+    lines  = [line.rstrip() for line in file][3:]
+
+# Remove build number
+lines = list(map(lambda x: drop_builds(x), lines))
+
+# Save the file
+with open(filename, mode='wt', encoding='utf-8') as myfile:
+    myfile.write('\n'.join(lines))
