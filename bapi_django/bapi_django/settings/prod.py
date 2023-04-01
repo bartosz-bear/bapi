31085fb67c1d336fd888257f63519c80d17cbf33
bartosz-bear <bartosz.artur.piechnik@gmail.com>
2023-04-01 14:05:17 +0200

    additional configuration for whitenoise

--- bapi_django/bapi_django/settings/prod.py
+++ bapi_django/bapi_django/settings/prod.py
@@ -11,7 +11,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 LOGGING = {
     "version": 1,
